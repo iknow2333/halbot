c0fc6298bae95921b2dc4ada6b963374d913aee3
mccoysc <364589886@qq.com>
2023-05-10 10:04:22 +0800

    Update halbot.mjs

--- bin/halbot.mjs
+++ bin/halbot.mjs
@@ -2,15 +2,13 @@
 
 import { cache, dbio, memory, storage as _storage, utilitas } from 'utilitas';
 import halbot from '../index.mjs';
-import * as fs from "fs";
 
-if(!fs.existsSync("~/.halbot.json")){
-    fs.writeFileSync("~/.halbot.json","{}");
-}
+const envConfig=JSON.parse(process.env.BOT_CONFIG || "{}")
+
 const debug = utilitas.humanReadableBoolean(process.env['DEBUG']);
 const log = content => utilitas.log(content, import.meta.url);
 const MEMORY = 'memory';
-const _getConfig = async () => await _storage.getConfig();
+const _getConfig = async () => await _storage.getConfig({config:process.env.BOT_CONFIG_FILE|| "./config.json"});
 const getConfig = async key => (await _getConfig())?.config?.[key];
 
 let storage = {
@@ -19,8 +17,12 @@ let storage = {
 };
 
 try {
-    const { filename, config } = await _getConfig();
-    assert(utilitas.countKeys(config), `Error loading config from ${filename}.`);
+    var { filename, config } = await _getConfig();
+    if(!utilitas.countKeys(config)){
+        config=envConfig;
+    }
+    Object.assign(config,envConfig);
+    assert(utilitas.countKeys(config), `Error loading config from ${filename}. and ${process.env.BOT_CONFIG}`);
     const sessionType = utilitas.trim(config.storage?.type, { case: 'UP' });
     if (config.storage?.type) { delete config.storage.type; }
     switch (sessionType) {
